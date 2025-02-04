d1e986ad18c00f8d64ef85fd4da5e1bc93648684
Hedvig <carlevag@gmail.com>
2025-02-04 19:45:07 +0100

    Complete layout
    - Responsive
    + Complete layout
    
    Minor changes needed

--- src/components/hero-section.jsx
+++ src/components/hero-section.jsx
@@ -1,17 +1,41 @@
-const images = [
-  { src: "src/images/hot.jpg"},
-  { src: "src/images/juicy.jpg"},
-  { src: "src/images/cosy.jpg"},
+const imagesWithTitles = [
+  { src: "src/images/hot.jpg", 
+    title: "Hot", 
+    text1: "Hot freshly ground black coffee or a cup of equisite tea?", 
+    text2: "We give you that perfect cup every time",
+    menu: "Mocha Latte   € 7.50<br>" +
+          "Caffe Formaggio   € 5.00<br>" +
+          "Espresso   € 3.50<br>" +
+          "Chai Verde Latte   € 5.50"},
+
+  { src: "src/images/juicy.jpg", 
+    title: "Juicy",
+    text1: "Ripe fruit - freshly squeezed.",
+    text2: "It's as simple as that. Chunky or smooth - it's your choice.",
+    menu: "Branched Apricots   € 4.20<br>" +
+          "Deep Rasberries   € 3.50<br>" +
+          "Smooth Oranges   € 6.50"},
+
+  { src: "src/images/cosy.jpg", 
+    title: "Cozy",
+    text1: "Hang around. Enjoy the settings.",
+    text2: "Use our fast WiFi. Borrow a newspaper or a novel.",
+    menu: "Mon-Sun   8am – 11pm<br>" +
+          "Caffe Retro  Canto VI<br>" +
+          "0123-45 67 89   caffe@lorem.pge"},
 ];
 
-const titles = ["Hot", "Juicy", "Cozy"];
 
 export default function HeroSection() {
   return (
     <>
-      {images.map((image, index) => (
+      {imagesWithTitles.map((image, index) => (
         <div key={index} className="section">
           <img src={image.src} alt={image.alt} />
+          <div className="image-title">{image.title}</div>
+          <div className="image-text1">{image.text1}</div>
+          <div className="image-text2">{image.text2}</div>
+          <div className="image-menu" dangerouslySetInnerHTML={{ __html: image.menu }} />
         </div>
       ))}
     </>
